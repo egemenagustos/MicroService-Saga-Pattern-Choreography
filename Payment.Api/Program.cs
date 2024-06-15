9829e53e90bd709c35c57a3be1797f84cf0cd004
Egemen Agustos <info@egemenagustos.com>
2024-06-15 11:21:33 +0300

    added StockReservedEventConsumer for payment api

--- Payment.Api/Program.cs
+++ Payment.Api/Program.cs
@@ -1,12 +1,18 @@
 using MassTransit;
+using Payment.Api.Consumers;
+using Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMassTransit(configurator =>
 {
+    configurator.AddConsumer<StockReservedEventConsumer>();
+
     configurator.UsingRabbitMq((context, config) =>
     {
         config.Host(builder.Configuration["RabbitMq"]);
+
+        config.ReceiveEndpoint(RabbitMqSettings.Payment_StockReservedEventQueue, e => e.ConfigureConsumer<StockReservedEventConsumer>(context));
     });
 });
 
